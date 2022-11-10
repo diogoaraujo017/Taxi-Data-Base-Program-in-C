54a4d7544c0b41fe470005eef0b12bf8411f748f
TomasUM <tumabarbosa@gmail.com>
2022-11-10 16:12:58 +0000

    Criação de 2 ficheiros novos: rides.h e rides.c e desenvolvimento no código

---
+++ trabalho-pratico/includes/rides.h
@@ -0,0 +1,23 @@
+#ifndef RIDES
+#define RIDES
+
+
+
+typedef struct rides{
+    char *id;
+    char *date;
+    char *driver;
+    char *user;
+    char *city;
+    int distance;
+    int score_user;
+    int score_driver;
+    double tip;
+    char *comment;  
+}Rides;
+
+Rides init_ride(const char *str);
+char* get_date(Rides r);
+char* get_ride_info(Rides r);
+
+#endif
